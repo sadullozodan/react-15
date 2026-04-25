51ba4df5c78cbde081983071ab6e4dae7f7bd9b2
sadullozodan <nuriddinsadullozoda@gmail.com>
2026-04-26 04:44:28 +0500

    add

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import "./lib/i18n/I18n"
+import "./lib/i18n/I18n"
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
